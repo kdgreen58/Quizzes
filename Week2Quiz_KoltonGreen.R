c1160d77193e07dfc25e30d4fe234b68d4c79e28
kdgreen58 <kolton.green@spsmail.cuny.edu>
2014-09-05 22:53:28 -0400

    Week 2 Quiz Answers

---
+++ Week2Quiz_KoltonGreen.R
@@ -0,0 +1,32 @@
+#======================================================================================
+#Name: Kolton D. Green
+#Date: 9/5/2014
+#Course: IS 360 02
+#Week 2 Quiz
+#Summary: Provide answers to elevan questions written in R Script and uploaded to Github.
+#===================================================================================
+#1. x<-c(1,1,2,4,5,5,3,3,3,6,7,7,7,8,8,8,8,9,9,1)
+#2. y<-as.character(x)
+#3. z<-as.factor(x)
+#4. levels(z)
+#5. a<-(3*x^2-4*x-1)
+#6. names(ROYGBIV)<-c("Red","Orange","Yellow","Green","Blue","Indigo","Violet")
+#7. d<-c("A","B","C","D","B","C","F","A","C","A")
+#   e<-factor(c("Cat","Dog","Fish","Fish","Dog","Dog","Dog","Cat","Fish","Cat"))
+#   f<-c(15,20,11,13,20,19,16,15,13,9)
+#   g<-as.Date(c("2012-07-08","2011-09-08","2009-12-31","2006-03-22","2001-04-27","2013-08-16","2014-01-19","2004-05-06","2001-05-02","2009-08-22"))
+#   obserGrade<-d
+#   animal<-e
+#   min<-f
+#   birthdate<-g
+#   behavior<- data.frame(obserGrade,animal,min,birthdate)
+#8. animal<-factor(animal, levels= c(levels(animal), "Rat"))
+#   behavior<- data.frame(obserGrade,animal,min,birthdate)
+#   new<-c("B","Rat",12,"2014-05-06")
+#   behavior<-rbind(behavior,new)
+#9. temp<-read.csv("temperatures.csv")
+#10. setwd("d:/somethingelse")
+#    measure<-read.table("measurements.txt")
+#11. fake<- read.table("http://www.madeupdata.com/data.txt", sep="|")
+
+
