31a7b829350e816155eab71a3a4d2eee0bc5fc0b
Kolton Green <kolton.green@spsmail.cuny.edu>
2014-09-12 23:45:51 -0400

    Week 3 Quiz

---
+++ Week3Quiz_KoltonGreen.R
@@ -0,0 +1,43 @@
+#======================================================================================
+#Name: Kolton D. Green
+#Date: 9/12/2014
+#Course: IS 360 02
+#Week 3 Quiz
+#Summary: Provide answers to 13 questions written in R Script and upload to Github.
+#===================================================================================
+#1. x <- 1
+#    for(i in 1:12){
+#     x <- x*((1:12)[i])
+#    }
+#    print (x)
+#2. x <- 1500
+#   y <- 1
+#   while(y <= 72 ){
+#     x <- x*(1+(0.0324/12))
+#     y<- y+1
+#   }
+#   round(x, digits=2)
+#3. x <- as.numeric(1:20)
+#   y <- 0
+#   for (i in x){
+#     y<- y+3
+#   for (i in x[y]){
+#     z <- sum(x[y], na.rm = TRUE)
+#   }
+#   }
+#4.for (i in 1:10){
+#   x<- 2
+#   x<- x ^ i
+#  }
+#  print (x)
+#5. while (i <= 10){
+#     x<- 2
+#     x<- x ^ i
+#     i <- i + 1
+#   }
+#   print (x)
+#6. 
+#7. seq(20, 50, 5)
+#8. x<- rep("example", each = 10)
+#   x<- as.character(x)
+#9. 
