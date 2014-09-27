547b1bcc6e6c89f48d04b3f8c36632f0ea7e954f
Kolton Green <kolton.green@spsmail.cuny.edu>
2014-09-26 23:17:42 -0400

    Week5 Quiz

---
+++ Week5Quiz_KoltonGreen.R
@@ -0,0 +1,30 @@
+#======================================================================================
+#Name: Kolton D. Green
+#Date: 9/26/2014
+#Course: IS 360 02
+#Week 5 Quiz
+#Summary: Provide answers to 8 questions written in R Script and upload to Github.
+#======================================================================================
+#Tidy Data
+#1. 'Tidying'
+#2. 'Each variable forms a column' and 'Each observation forms a row'
+#3. 1) Column headers are values, not variable names
+#   2) Multiple Variables are stored in one column.
+#   3) Variables are stored in both rows and columns.
+#   4) Multiple types of observational units are stored in the same table.
+#   5) A single observational unit is stored in multiple tables.
+#4. (Definitions copied from text)
+#   Filter: subsetting or removing observations based on some condition(s)
+#   Transform: adding or modifying variables.
+#   Aggregate: collapsing multiple values into a single value.
+#   Sort: changing the order of observations
+#5. subset() and transform()
+#
+# Introduction to dplyr
+#1. Advantages: dplyr is faster and better thought out set of joins
+#   Disadvantages: only works with data frames
+#2. Advantages: More consisten
+#   Disadvantages: Centered around only data frames
+#
+#tidyr
+#1. gather() and spread()
