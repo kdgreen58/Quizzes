b9b5a9c4c89076e507bf376f25ac9c7d02ff21a7
Kolton Green <kolton.green@spsmail.cuny.edu>
2014-10-10 23:30:11 -0400

    Week 7 Quiz

---
+++ Week7Quiz_KoltonGreen.R
@@ -0,0 +1,16 @@
+#======================================================================================
+#Name: Kolton D. Green
+#Date: 10/10/2014
+#Course: IS 360 02
+#Week 7 Quiz
+#Summary: Provide code to 4 questions in R Script and upload to Github.
+#======================================================================================
+#1. Reading in two files (delimited differently) that have dates using two different
+#   date formats.
+#2. Quoting from the RMarkdown page (http://rmarkdown.rstudio.com) "They can be
+#   automatically regenerated whenever underlying R code or data changes."
+#3. Just because something is correlated (such as something showing correlation
+#   on a scatterplot) doesn't mean one causes the other.
+#4. From my experience variable coding is helpful when you have column names stored
+#   in a variable and you need to change just certain ones while maintaining other, or
+#   changing a factor name while maintaining the others.
