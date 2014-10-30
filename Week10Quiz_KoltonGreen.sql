c3f1c58d7d6c59c0248c0540b4c92bfe6a5fbbc1
Kolton Green <kolton.green@spsmail.cuny.edu>
2014-10-30 19:27:04 -0400

    Week 10 Quiz

---
+++ Week10Quiz_KoltonGreen.sql
@@ -0,0 +1,10 @@
+﻿SELECT last_name
+FROM customer
+WHERE left(last_name, 1) = 'M' or left(last_name, 1) = 'N';
+
+SELECT MAX(length)
+FROM film;
+
+SELECT DISTINCT category_id
+FROM category
+WHERE name = 'Animation' or name = 'Children' or name = 'Family';
